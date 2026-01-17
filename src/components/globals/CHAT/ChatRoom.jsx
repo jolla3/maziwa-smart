fa8f246273294d838daaff84fc8ba253d0415382
joll3a <its.jolla6@gmail.com>
2026-01-17 20:50:32 +0300

    v28.3

--- src/components/globals/CHAT/ChatRoom.jsx
+++ src/components/globals/CHAT/ChatRoom.jsx
@@ -13,6 +13,7 @@ import {
 import EmojiPicker from "emoji-picker-react";
 import { AuthContext } from "../../PrivateComponents/AuthContext";
 
+
 const API_BASE = process.env.REACT_APP_API_BASE;
 const SOCKET_URL = process.env.REACT_APP_API_BASE;
 const MESSAGE_CACHE_PREFIX = "chat_messages_";
